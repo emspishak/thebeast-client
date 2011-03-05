3fb33e811a1aca97840e6a51365e0429974230fe
Eric Spishak <emspishak@gmail.com>
2011-03-05 08:48:46 -0800

    added update success message

--- upload_common.php
+++ upload_common.php
@@ -1,30 +1,34 @@
 <?php
 function display_non_matches($result) {
 	?>
-	<div id="results">
-		<h2>I couldn't figure out what movie you meant with the following files. Please select the correct movie or enter a better movie name (you might want to find the exact name from IMDB.)</h2>
-		<form method="post" action="upload_again.php">
-			<?php
-			foreach ($result->non_matches as $non_match) {
-				?>
-				<div class="nonmatch">
-					Filename: <?= $non_match->filename ?><br />
-					<?php
-					foreach ($non_match->matches as $match) {
-						$filename = preg_replace("/ /", "%%%", $non_match->filename);
-						?>
-						<label><input type="radio" name="<?= $filename ?>" value="<?= $match->name ?>;;;<?= $match->year ?>" />Possible match: <?= $match->name ?> (<?= $match->year ?>)</label><br />
-						<?php	
-					}
-					?>
-					<label><input type="radio" name="<?= $filename ?>" value="entered_own" />Or, Enter your own name</label><input type="text" name="<?= $filename ?>;;;own" />
-				</div>
+		<div id="results">
+		if (count($result->non_matches) == 0) {
+			<h2>Congratsies! You successfully added all of your movies!</h2>
+		} else {
+			<h2>I couldn't figure out what movie you meant with the following files. Please select the correct movie or enter a better movie name (you might want to find the exact name from IMDB.)</h2>
+			<form method="post" action="upload_again.php">
 				<?php
-			}
-			?>
-			<input type="submit" />
-		</form>
-	</div>
+				foreach ($result->non_matches as $non_match) {
+					?>
+					<div class="nonmatch">
+						Filename: <?= $non_match->filename ?><br />
+						<?php
+						foreach ($non_match->matches as $match) {
+							$filename = preg_replace("/ /", "%%%", $non_match->filename);
+							?>
+							<label><input type="radio" name="<?= $filename ?>" value="<?= $match->name ?>;;;<?= $match->year ?>" />Possible match: <?= $match->name ?> (<?= $match->year ?>)</label><br />
+							<?php	
+						}
+						?>
+						<label><input type="radio" name="<?= $filename ?>" value="entered_own" />Or, Enter your own name</label><input type="text" name="<?= $filename ?>;;;own" />
+					</div>
+					<?php
+				}
+				?>
+				<input type="submit" />
+			</form>
+		}
+		</div>
 	<?php
 }
 ?>
