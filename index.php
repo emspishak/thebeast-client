c2cac9c899f2761af7d8e38a7fef94efa59cdd5f
Eric Spishak <emspishak@gmail.com>
2011-03-04 18:20:12 -0800

    added index (basic signin page

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title>Cinesaurus Server Administration</title>
+	</head>
+	
+	<body>
+		<h1>Cinesaurus Server Administration</h1>
+		<h2>Log in with your Cinesaurus account</h2>
+		<form method="POST" action="login.php">
+			<label>User name:<input type="text" name="username" /></label>
+			<label>Password:<input type="password" name="password" /></label>
+			<input type="submit" />
+		</form>
+	</body>
+</html>
