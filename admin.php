18598bc15f0f64d6da2b78be383baf6255f5ba78
Eric Spishak <emspishak@gmail.com>
2011-03-04 21:28:17 -0800

    added correct id for requests

--- admin.php
+++ admin.php
@@ -4,7 +4,7 @@ include("common.php");
 
 html_header();
 
-$r = new HttpRequest('index.php', HttpRequest::METH_POST);
+$r = new HttpRequest('http://ec2-75-101-227-4.compute-1.amazonaws.com/client_login', HttpRequest::METH_POST);
 $r->addPostFields(array('username' => $_REQUEST["username"], 'password' => $_REQUEST["password"]));
 $r->send();
 $response = $r->getResponseBody();
