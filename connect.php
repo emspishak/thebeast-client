35c4d7d7012372143870c61f62ec107f139f253c
Eric Spishak <emspishak@gmail.com>
2011-03-04 23:08:58 -0800

    debug

--- connect.php
+++ connect.php
@@ -4,7 +4,9 @@ include("common.php");
 $session_key = check_logged_in();
 
 $r = new HttpRequest($site_root . "/register_client");
-$r->addPostFields(array("session_id" => $session_key, "name" => $_REQUEST["client_name"]));
+$params = array("session_id" => $session_key, "name" => $_REQUEST["client_name"]);
+print_r($params);
+$r->addPostFields($params);
 $r->send();
 $response = json_decode($r->getResponseBody());
 print_r($r->getResponseBody());
