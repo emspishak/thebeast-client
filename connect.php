123fa603489eb85ae281c42d5d0d4d0f5220b043
Eric Spishak <emspishak@gmail.com>
2011-03-04 23:16:59 -0800

    fix?

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@
 include("common.php");
 $session_key = check_logged_in();
 
-$r = new HttpRequest($site_root . "/register_client");
+$r = new HttpRequest($site_root . "/register_client", HttpRequest::METH_POST);
 $params = array('session_id' => $session_key, 'name' => $_REQUEST["client_name"]);
 print_r($params);
 $r->addPostFields($params);
